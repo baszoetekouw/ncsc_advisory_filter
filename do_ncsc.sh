79fe80378274f192998f5e0b779f784268e75dfd
Bas Zoetekouw <bas@juliet.lan>
2022-05-05 14:06:42 +0200

    added service files

--- do_ncsc.sh
+++ do_ncsc.sh
@@ -2,8 +2,10 @@
 
 set -e
 
+BASEDIR=$( dirname -- "${BASH_SOURCE[0]}" )
+
 wget -q -O input.rss  https://advisories.ncsc.nl/rss/advisories
-xsltproc ncsc.xslt input.rss > output.rss
+xsltproc ${BASEDIR}/ncsc.xslt input.rss > output.rss
 mv output.rss ncsc_new.rss
 
 exit 0
